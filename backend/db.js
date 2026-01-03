2dc8688cd6161eb53f888d60e6de0f2a95e13d61
Sahana240107 <sahana240107@gmail.com>
2026-01-03 10:13:03 +0530

    MySQL connection

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,15 @@
+const mysql = require('mysql2');
 
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'YOUR_MYSQL_PASSWORD',
+  database: 'globetrotter'
+});
+
+db.connect(err => {
+  if (err) throw err;
+  console.log('MySQL Connected');
+});
+
+module.exports = db;
